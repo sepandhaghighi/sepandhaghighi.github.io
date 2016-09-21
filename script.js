d45ba8adfe0bca720bc81a847cd02f7017098a20
sepandhaghighi <sepand.haghighi@yahoo.com>
2016-09-22 00:46:17 +0330

    animation added

--- script.js
+++ script.js
@@ -24,13 +24,15 @@ var image_text=["With Sarmin (Tehran - 2016)",
                 "With Mostafa ( Roodbar - 2016)" ];
                 
 var index = 0;
+var time_1;
+var time_2;
 
 function next() {
     var slider = document.getElementById("slidebar");
     var text=document.getElementById("image_text");
     index=index+1;
     if (index==image_arr.length){
-        index=0
+        index=0;
     }
     slider.src = image_arr[index];
     text.innerHTML=image_text[index];
@@ -43,12 +45,43 @@ function prev() {
      var text=document.getElementById("image_text");
     index=index-1;
     if (index<0){
-        index=image_arr.length-1
+        index=image_arr.length-1;
     }
     slider.src = image_arr[index];
      text.innerHTML=image_text[index];
     
 }
+function colorchange1(a){
+    var but=document.getElementsByTagName("button");
+    if (a==1) {
+         but[0].style.background="red";
+    }
+   else{
+       but[1].style.background="red";
+   }
+    
+    
+        
+}
+
+    
+
+function originalcolor(a){
+    var but=document.getElementsByTagName("button");
+    if (a==1){
+         but[0].style.background="orange";
+
+    }
+    else{
+          but[1].style.background="orange";
+
+    }
+   
+  
+    
+}
+  
+ 
 
 setInterval(next,15000);
 
